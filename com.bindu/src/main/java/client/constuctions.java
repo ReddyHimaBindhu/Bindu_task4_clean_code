52af81be83d4cc4a6f298c319374b9fa90dd9bc3
ReddyHimaBindhu <60332709+ReddyHimaBindhu@users.noreply.github.com>
2020-02-20 22:30:13 +0530

    Add files via upload

---
+++ com.bindu/src/main/java/client/constuctions.java
@@ -0,0 +1,5 @@
+package client;
+
+public interface constuctions {
+	String Constructions_Materials(String Material_Standard,int Area );
+}
---
+++ com.bindu/src/main/java/p_layer/present.java
@@ -0,0 +1,5 @@
+package p_layer;
+public interface present {
+	int Compound_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+	int Simple_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+}
